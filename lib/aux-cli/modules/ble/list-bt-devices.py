cb8b6c3c279aa4769736f6c9f92b64d6ce9cb754
DevOps YETi <devops@yeticgi.com>
2019-11-12 10:28:05 -0500

    [Wesley] bluetooth hotspot should be fully functioning now. currently checks every 5 seconds for a bluetooth connection

--- lib/aux-cli/modules/ble/list-bt-devices.py
+++ lib/aux-cli/modules/ble/list-bt-devices.py
@@ -4,11 +4,13 @@ import os
 import time
 from pybtooth import BluetoothManager
 
+SCRIPT = os.path.abspath(__file__)
+SCRIPTPATH = os.path.dirname(SCRIPT)
 devices = BluetoothManager().getConnectedDevices(no_name=True)
 
 print "Found " + str(len(devices)) + " devices."
 
 for device in devices:
     addr = device.get("Address")
-    os.system("sudo sh check-and-connect-pt-pan.sh " + addr)
+    os.system("sudo sh " + SCRIPTPATH + "/check-and-connect-pt-pan.sh " + addr)
     time.sleep(3)
