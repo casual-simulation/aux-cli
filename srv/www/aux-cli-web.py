541cc35aecd2d32e290fadc504a13dae3ddb3d1d
Wesley <wesley@yeticgi.com>
2020-10-30 17:43:10 -0400

    fix: POST data decode to utf8 to work

--- srv/www/aux-cli-web.py
+++ srv/www/aux-cli-web.py
@@ -1,11 +1,12 @@
 from flask import Flask, request
+import subprocess
  
 app = Flask(__name__)
  
 @app.route('/post', methods = ["POST"])
 def post():
-    if request.data == "connect":
-        os.system('/usr/bin/env bash /bin/bt-serial-connect')
+    if request.data.decode('utf-8') == "connect":
+        subprocess.call('/bin/bt-serial-connect')
     return ''
  
 app.run(host='0.0.0.0', port= 8090)
