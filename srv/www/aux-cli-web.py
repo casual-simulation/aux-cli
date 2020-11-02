1b7b4229257399a889a2f16f493b6ab9c1615635
Wesley <wesley@yeticgi.com>
2020-11-02 13:58:41 -0500

    feat: add a disconnect option for bluetooth serial

--- srv/www/aux-cli-web.py
+++ srv/www/aux-cli-web.py
@@ -5,8 +5,14 @@ app = Flask(__name__)
  
 @app.route('/post', methods = ["POST"])
 def post():
+    # Connect the bluetooth serial device
     if request.data.decode('utf-8') == "connect":
         subprocess.call('/bin/bt-serial-connect')
+
+    # Disconnect the bluetooth serial device
+    if request.data.decode('utf-8') == "disconnect":
+        subprocess.call('/bin/bt-serial-disconnect')
+
     return ''
  
 app.run(host='0.0.0.0', port= 8090)
