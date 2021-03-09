bd7155998281db174a0fc7691849d1dcab4bdff2
Wesley <wesley@yeticgi.com>
2021-03-09 13:51:04 -0500

    feat: revamped flask server for serial connections

--- srv/www/auxcli-web.py
+++ srv/www/auxcli-web.py
@@ -1,17 +1,25 @@
 from flask import Flask, request
+import json
 import subprocess
  
 app = Flask(__name__)
  
 @app.route('/post', methods = ["POST"])
 def post():
+
+    data = json.loads(request.data)
+    command = data['command']
+    device = data['device']
+    mac = data['mac']
+    channel = data['channel']
+
     # Connect the bluetooth serial device
-    if request.data.decode('utf-8') == "connect":
-        subprocess.call('/lib/auxcli/helper/bt-serial-connect')
+    if command == "connect":
+        subprocess.call(['/lib/auxcli/helper/bt-serial-connect', device, mac, channel])
 
     # Disconnect the bluetooth serial device
-    if request.data.decode('utf-8') == "disconnect":
-        subprocess.call('/lib/auxcli/helper/bt-serial-disconnect')
+    if command == "disconnect":
+        subprocess.call(['/lib/auxcli/helper/bt-serial-connect', device])
 
     return ''
  
