8cd0505ad84dcf0ec80c64a97428f094b01d8747
Wesley <wesley@yeticgi.com>
2021-03-19 10:57:26 -0400

    chore: more notes on serial setup for now

--- temp-stuff/aux-cli/brushsim-tmp-01.sh
+++ temp-stuff/aux-cli/brushsim-tmp-01.sh
@@ -74,3 +74,94 @@ npm install johnny-five raspi-io
 #######################################################
 
 hcitool scan | grep RNBT | awk '{print $1}' | sudo rfcomm connect hci0 1 
+
+
+# PIPELINE TESTING STUFF
+#######################################################
+
+# test.js
+# const execSync = require('child_process').execSync;
+# let device = "/dev/rfcomm0";
+# let mac = "00:06:66:B9:08:47";
+# let channel = 1;
+
+# console.log(device);
+# console.log(mac);
+# console.log(channel);
+
+# let jsond = '{"command":"connect","device":"' + device + '", "mac":"' + mac + '", "channel":"' + channel + '"}';
+# console.log(jsond);
+# let curl_command = 'curl -H "Content-Type: application/json" -X POST -d \'' + jsond + '\' http://192.168.1.18:8090/post';
+# console.log(curl_command);
+
+# execSync(curl_command);
+
+# console.log("done");
+
+
+
+# test.py
+# import subprocess
+# import json
+
+
+# mdata = '{"command":"connect","device":"/dev/rfcomm0","mac":"00:06:66:B9:08:47","channel":"1"}'
+# data = json.loads(mdata)
+
+# command = (data['command'])
+# # command = "connect"
+# device = (data['device'])
+# # device = "/dev/rfcomm0"
+# mac = (data['mac'])
+# # mac = "00:06:66:B9:08:20"
+# channel = (data['channel'])
+# # channel = "1"
+
+# if command == "connect":
+#     # device_c = (data['device'])
+#     # mac = (data['mac'])
+#     # channel = (data['channel'])
+#     subprocess.call(['/usr/bin/timeout', '60s', '/lib/auxcli/commands/helper/bt-serial-connect', device, mac, channel])
+
+
+
+#btscan.sh
+
+#!/bin/bash
+
+# readarray -t current_devices < <(jq -r '.[].mac' /home/pi/test.json)
+# # echo ${current_devices[@]}
+
+# scan_devices(){
+#     readarray -t discovered_devices < <(hcitool scan)
+#     echo ${discovered_devices[@]}
+#     for ((i = 1; i < ${#discovered_devices[@]}; i++)); do
+#             device_mac=$(echo ${discovered_devices[i]} | awk '{print $1}')
+#             device_name=$(echo ${discovered_devices[i]} | awk '{print $2}')
+#             if [[ ! ${current_devices[*]} =~ $device_mac ]]; then
+#                 # Add a new device
+#                 jq --arg mac "$device_mac" --arg name "$device_name" --arg desc "$device_desc" '. += [{"mac": $mac, "name": $name, "desc": $desc}]' /home/pi/test.json | sudo tee /home/pi/test.$
+#                 sudo mv -f /home/pi/test.json.tmp /home/pi/test.json
+#             fi
+#             readarray -t current_devices < <(jq -r '.[].mac' /home/pi/test.json)
+#             if [ ! $device_name == n/a ]; then
+#                 if [[ ! ${current_devices[*]} =~ $device_name ]]; then
+#                     jq --arg mac "$device_mac" --arg name "$device_name" '(.[] | select (.mac == $mac) | .name) = $name' /home/pi/test.json | sudo tee /home/pi/test.json.tmp 1> /dev/null
+#                     sudo mv -f /home/pi/test.json.tmp /home/pi/test.json
+#                 fi
+#             fi
+#     done
+# }
+
+# first_scan(){
+#     while [ ${#current_devices[@]} -eq 0 ]; do
+#         echo "Empty!"
+#         scan_devices
+#         readarray -t current_devices < <(jq -r '.[].mac' /home/pi/test.json)
+#     done
+# }
+
+# first_scan
+# scan_devices
+
+
